e8884acb8ce6b8c82d041bd3011efdd3aaf2c184
hvu05 <hoangvu06052005@gmail.com>
2025-02-20 23:10:09 +0700

    first commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,8 @@
+function Menu() {
+    return (
+        <>
+        Los menús son una parte esencial de cualquier aplicación web, ya que proporcionan una manera intuitiva y accesible para que los usuarios naveguen y accedan a diferentes secciones o funcionalidades. Aquí hay algunos puntos clave sobre los menús en aplicaciones web:
+        </>
+    )
+}
+export default Menu;
